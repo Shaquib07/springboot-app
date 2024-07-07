780e69ee604e3846696dfa023a146a429c7aedb5
Shaquib07 <mdshaquibjawaid01@gmail.com>
2024-07-07 20:22:22 +0530

    project structure commit

--- src/main/java/com/pack/SpringBootBasicsApplication.java
+++ src/main/java/com/pack/SpringBootBasicsApplication.java
@@ -2,14 +2,23 @@ package com.pack;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(value="com.pack")
-public class SpringBootBasicsApplication {
+public class SpringBootBasicsApplication extends SpringBootServletInitializer {
+
+	/*public static void main(String[] args) {
+		SpringApplication.run(SpringBootBasicsApplication.class, args);
+	}*/
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringBootBasicsApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootBasicsApplication.class, args);
 	}
-
 }
--- src/main/java/com/pack/controller/UserController.java
+++ src/main/java/com/pack/controller/UserController.java
@@ -10,6 +10,7 @@ public class UserController {
 
     @GetMapping("/login")
     public String loginPage(){
+        System.out.println("Into login function...");
         return "login";
     }
 }
